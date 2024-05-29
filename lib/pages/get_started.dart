d87c2b9bc642a17dfd07077f01efaddc2e452283
Abirami <abirami.anandkumar1975@gmail.com>
2024-05-29 23:10:01 +0530

    Descriptive commit message

--- lib/pages/get_started.dart
+++ lib/pages/get_started.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class GetStarted extends StatefulWidget {
-   GetStarted({super.key});
+  GetStarted({super.key});
 
   @override
   State<GetStarted> createState() => _GetStartedState();
@@ -15,23 +15,40 @@ class _GetStartedState extends State<GetStarted> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-       children: [
+      children: [
         BackgroundImage(),
         Scaffold(
           backgroundColor: Colors.transparent,
           body: Center(
             child: Padding(
-              padding: const EdgeInsets.only(top: 200.0),
+              padding: const EdgeInsets.only(top: 250.0),
               child: Column(
                 children: [
-                  Text('ASPIRE ARC',style: GoogleFonts.poppins(fontWeight:FontWeight.bold,fontSize:30,color:Color(0xffEBDEF0)),)
+                  Text(
+                    'ASPIRE ARC',
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 33,
+                        color: Color(0xffF4ECF7)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'Embark on a seamless journey to professional growth. AspireArc is your trusted partner in navigating the arc of your career, offering personalized tools and insights to help you achieve your highest aspirations. Let’s begin the path to your future success.',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                        color: Color(0xffF4ECF7),
+                       
+                      ), textAlign: TextAlign.center,
+                    ),
+                  ),
                 ],
-              
               ),
             ),
           ),
         ),
-       ],
+      ],
     );
   }
 }

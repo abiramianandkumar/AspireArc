3a78bc83d88e8bf63e3fa2538b1fc01cc8ce244f
ganesh20060705 <ganeshdeepak65@gmail.com>
2024-05-29 23:05:57 +0530

    Descriptive commit message

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:aspire_arc/components/bgimage.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+          BackgroundImage(),
+       ],
+      
+    );
+  }
+}
+

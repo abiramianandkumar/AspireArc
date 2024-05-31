824886512d2ff120944cf1a672439c83fac28f21
ganesh20060705 <ganeshdeepak65@gmail.com>
2024-05-31 19:59:33 +0530

    Descriptive commit message

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,22 +1,54 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:aspire_arc/components/bgimage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class loginpage extends StatefulWidget {
+  loginpage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<loginpage> createState() => _loginpageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _loginpageState extends State<loginpage> {
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-          BackgroundImage(),
-       ],
-      
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 250.0),
+              child: Column(
+                children: [
+                  Text(
+                    'LOGIN',
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 33,
+                        color: Color(0xffF4ECF7)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'ss.',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                        color: Color(0xffF4ECF7),
+                       
+                      ), textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
-

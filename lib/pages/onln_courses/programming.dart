3486e7a55a33ebe06f3f6e9f4cb1fa075d479cc2
Abirami <abirami.anandkumar1975@gmail.com>
2024-07-09 23:08:46 +0530

    Descriptive commit message

--- lib/pages/onln_courses/programming.dart
+++ lib/pages/onln_courses/programming.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Programming extends StatelessWidget {
@@ -10,24 +11,39 @@ class Programming extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xffF4ECF7 ),
+        backgroundColor: Color(0xffF4ECF7),
         title: Text(
           'Programming Courses',
-          
           style: GoogleFonts.poppins(),
         ),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(children: [
-              Container(height: 100,decoration: BoxDecoration(color: Colors.cyan),child: Column(children: [
-                  
-
-              ],),)
-            ],)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 100,
+           width: double.infinity,
+            decoration: BoxDecoration(color: Colors.cyan),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('hjgj'),
+                Text('hjvhv'),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('hjgj',style:TextStyle(fontSize: 40),),
+            ],
+          ),
+              ],
+            ),
+          ),
         ),
       ),
     );
